b096375dbbb3d9a89bda0cdf07259c52fdf2fee3
DJcuber <djcuber255@gmail.com>
2023-06-15 20:30:06 +0100

    3d with matrix

--- 3d.py
+++ 3d.py
@@ -11,15 +11,19 @@ class Game:
     self.running = True
   
   def run(self):
-    pyramid = Object3d("tetrahedron", 0, 0, 0, "#ff0000")
+    t = 0
+    cube = Object3d("tetrahedron", 0, 0, t, "#ff0000")
+    mat = matrix.Matrix4.perspecProjMat(1, -1, 1, -1, 1, 10)
+    points = self.translate(cube, mat)
     while self.running:
       self.window.fill("#000000")
-      pyramid.rotation += 0.1
-      if pyramid.rotation > 2 * math.pi:
-        pyramid.rotation -= 2 * math.pi
+      t += 0.1
+      if cube.rotation > 2 * math.pi:
+        cube.rotation -= 2 * math.pi
+      cube = Object3d("tetrahedron", 0, 0, t, "#ff0000")
+      points = self.translate(cube, mat)
+      self.draw3d(cube, points)
 
-      pyramid.update()
-      self.draw3d(pyramid)
       self.eventHandle()
       pg.display.flip()
       self.clock.tick(10)
@@ -29,18 +33,26 @@ class Game:
       if ev.type == pg.QUIT:
         self.running = False 
   
-  def draw3d(self, obj):
-    point = []
-    for i in obj.vertices:
-      point.append((int(self.windowSize[0]*i[0]/i[2] + self.windowSize[0]/2), int(self.windowSize[1]*i[1]/i[2] + self.windowSize[1]/2)))
+  def draw3d(self, obj, point):
     for i in obj.edges:
-      pg.draw.aaline(self.window, obj.color, point[i[0]], point[i[1]])
+      pg.draw.aaline(self.window, obj.color, point[i[0]][:2], point[i[1]][:2])
         
+  def translate(self, obj, mat):
+    points = []
+    for i in obj.vertices:
+      vec = i.multiplyMat(mat)
+      newVec = [0]*3
+      newVec[0] = int((1 + (vec.vec[0]/vec.vec[3]))*self.windowSize[0]/2)
+      newVec[1] = int((1 + (vec.vec[1]/vec.vec[3]))*self.windowSize[1]/2)
+      newVec[2] = vec.vec[2]
+      points.append(newVec)
+    return points
+
 
 class Object3d:
-  def __init__(self, objType, pos, scale, rotation, color):
+  def __init__(self, objType, pos, scale, t, color):
     self.objType = objType
-    self.rotation = rotation
+    self.rotation = t
     self.color = color
     #self.vertices = [(x, y, z) for z in range(2) for y in range(2) for x in range(2)]
     if objType == "cube":
@@ -48,17 +60,14 @@ class Object3d:
       self.edges = [(0, 1), (0, 2), (0, 4), (1, 3), (1, 5), (2, 3), (2, 6), (3, 7), (4, 6), (4, 5), (5, 7), (6, 7)]
 
     if objType == "tetrahedron":
-      self.vertices = [(1/(2*math.sqrt(2)), 0.5, 3), (-1/(2*math.sqrt(2)), 0.5, 3), (0, 0, 2 + (math.sqrt(3))/4), (0, (1/2), (math.sqrt(3)/4)+ 2)]
+      #self.vertices = [matrix.Vector4((math.sin(t)/(2*math.sqrt(2)), 0, 3)), matrix.Vector4((-math.sin(t+1)/(2*math.sqrt(2)), 0, 3)), matrix.Vector4((math.sin(t+2), 0, 2 + (math.sqrt(3))/4)), matrix.Vector4((0, (1/2), (math.sqrt(3)/4)+ 2))]
+      self.vertices = [matrix.Vector4((math.sin(t+math.pi*4/3)/2, -0.15, 2+math.cos(t+math.pi*4/3)/2, 1)), matrix.Vector4((math.sin(t+math.pi*2/3)/2, -0.15, 2+math.cos(t+math.pi*2/3)/2, 1)), matrix.Vector4((math.sin(t)/2, -0.15, 2+math.cos(t)/2, 1)), matrix.Vector4((0, math.sqrt(3)/2, 2, 1))]
       self.edges = [(0, 1), (0, 2), (0, 3), (1, 2), (1, 3), (2, 3)]
 
-  def update(self):
-    if self.objType == "cube":
-      self.vertices = [(0.5, 0.5, 2), (0.5, 0.5, 3), (-0.5, 0.5, 2), (-0.5, 0.5, 3), (0.5, -0.5, 2), (0.5, -0.5, 3), (-0.5, -0.5, 2), (-0.5, -0.5, 3)] 
-      self.edges = [(0, 1), (0, 2), (0, 4), (1, 3), (1, 5), (2, 3), (2, 6), (3, 7), (4, 6), (4, 5), (5, 7), (6, 7)]
+    if self.objType == "cubeWithBones":
+      self.vertices = [matrix.Vector4((x-0.5+math.sin(t)/2, y-0.5, z + math.cos(t)/2, 1)) for x in range(2) for y in range(2) for z in range(2, 4)]
+      self.edges = [(i, j) for i in range(len(self.vertices)) for j in range(i, len(self.vertices)) if i != j]
 
-    if self.objType == "tetrahedron":
-      self.vertices = [(1/(2*math.sqrt(2)), 0.5, 3), (-1/(2*math.sqrt(2)), 0.5, 3), (0, 0, 2 + (math.sqrt(3))/4), (0, (1/2), (math.sqrt(3)/4)+ 2)]
-      self.edges = [(0, 1), (0, 2), (0, 3), (1, 2), (1, 3), (2, 3)]
 
   
 
@@ -70,4 +79,3 @@ def main():
 
 if __name__ == "__main__":
   main()
-
--- matrix.py
+++ matrix.py
@@ -30,18 +30,20 @@ class Vector4:
     y = (self.vec[0] * mat.mat[1][0]) + (self.vec[1] * mat.mat[1][1]) + (self.vec[2] * mat.mat[1][2]) + (self.vec[3] * mat.mat[1][3])
     z = (self.vec[0] * mat.mat[2][0]) + (self.vec[1] * mat.mat[2][1]) + (self.vec[2] * mat.mat[2][2]) + (self.vec[3] * mat.mat[2][3])
     w = (self.vec[0] * mat.mat[3][0]) + (self.vec[1] * mat.mat[3][1]) + (self.vec[2] * mat.mat[3][2]) + (self.vec[3] * mat.mat[3][3])
-    self.vec = [x, y, z, w]
+    return Vector4([x, y, z, w])
   
   def multiplyScalar(self, n):
-    self.vec[0] *= n
-    self.vec[1] *= n
-    self.vec[2] *= n
-    self.vec[3] *= n
+    newVec = [[]*4]
+    newVec[0] = self.vec * n
+    newVec[1] = self.vec * n
+    newVec[2] = self.vec * n
+    newVec[3] = self.vec * n
+    return newVec
 
 if __name__ == "__main__":
   #mat = Matrix4.perspecMat(1, 2)
   mat = Matrix4.perspecProjMat(1, -1, 1, -1, 1, 10)
-  vec = Vector4([6, 6, 3, 1])
+  vec = Vector4([0.5, 0.5, 2, 1])
   vec.multiplyMat(mat)
   vec.multiplyScalar(1/vec.vec[3])
   print(vec.vec)
